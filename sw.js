0a4b60b5be5663cc9b23ad3177a9b5ce7951c841
theus1990 <matheusrabelo9@hotmail.com>
2024-04-14 20:41:53 -0300

    Version 1.4

--- sw.js
+++ sw.js
@@ -1,3 +1,27 @@
+/*self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open('meu-portfolio-v1').then(function (cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/style.css',
+                '/script.js',
+                '/img/icon2.jpg'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+*/
+
 // This is the "Offline page" service worker
 
 importScripts(
